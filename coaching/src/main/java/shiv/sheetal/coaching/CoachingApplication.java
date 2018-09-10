07413f5df5dc5520870c804c46fa6b46eb914358
user <user@user-PC>
2018-09-10 12:40:48 +0530

    2nd commit

--- coaching/src/main/java/shiv/sheetal/coaching/CoachingApplication.java
+++ coaching/src/main/java/shiv/sheetal/coaching/CoachingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoachingApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(CoachingApplication.class, args);
 	}
