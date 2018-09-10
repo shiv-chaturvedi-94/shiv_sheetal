f829375dab1ebbb58744be977421835934f39674
user <user@user-PC>
2018-09-10 12:30:30 +0530

    first commit all files

---
+++ coaching/src/main/java/shiv/sheetal/coaching/CoachingApplication.java
@@ -0,0 +1,12 @@
+package shiv.sheetal.coaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachingApplication.class, args);
+	}
+}
---
+++ coaching/src/test/java/shiv/sheetal/coaching/CoachingApplicationTests.java
@@ -0,0 +1,16 @@
+package shiv.sheetal.coaching;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoachingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
